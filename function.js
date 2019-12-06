c267da62cc5cb77e8e015fddb0ef9557fb7eea7f
Tom Strat Laptop <tomstratfic@hotmail.com>
2019-12-06 16:43:35 +0000

    finalised reading and writing functionality ready for test

--- function.js
+++ function.js
@@ -7,47 +7,111 @@ const VIP_NAMES = ["Keytek", "The Aerial Man", "Digital TV", "Packsafe",
 "The Monmouthshire Window", "Monmouth Conservatories", "CGS",
  "L Miles", "LEL Scaffolding", "Yate Windows"];
 
-const ACCOUNT_MANAGERS = ["Graham Higgins", "Caroline Williams",
-"Jane Eveleigh", "Sam Bray", "Matthew Rose", "Dan Bernard",
-"Steve Wivell", "Julie Cole", "Joe Harris", "Joanne Collins", "Jo Hughes",
+const ACCOUNT_MANAGERS = ["Jane Eveleigh", "Sam Bray", "Matthew Rose",
+"Dan Bernard", "Julie Cole", "Joanne Collins", "Jo Hughes", "Amber Sharpe",
 "Amy Carlin", "Jacqui Lewis"];
 
 const AM_NUMBERS = {
-    "Graham Higgins":"01179630013",
-    "Caroline Williams":"01179630012",
     "Jane Eveleigh":"01179630011",
     "Sam Bray":"07584418002",
     "Dan Bernard":"01179231122",
-    "Steve Wivell":"07918088902",
     "Julie Cole":"07769256172",
-    "Joe Harris":"01179630019",
     "Joanne Collins":"07384254044",
     "Jo Hughes":"01179231122",
+    "Amber Sharpe":"01179630019",
     "Amy Carlin":"07503642097",
     "Jacqui Lewis":"01179630014"
 };
 
 
 
+//This section deals with reading in the CSV
+function handleFiles (files){
+    // Check for the various File API support.
+    if (window.File && window.FileReader) {
+      // Great success! All the File APIs are supported.
+      getAsText(files[0]);
+    } else {
+      alert('The File APIs are not fully supported in this browser.');
+    }
+  }
+  
+  function getAsText(fileToRead){
+    var reader = new FileReader();
+    // Read file into memory as UTF-8
+    reader.readAsText(fileToRead);
+    //Handle errors load
+    reader.onload = loadHandler;
+    reader.onerror = errorHandler;
+  }
+  
+  function loadHandler(event){
+    var csv = event.target.result;
+    processData(csv);
+  }
+  
+  function processData(csv){
+    var allTextLines = csv.split(/\r\n|\n/)
+    for (var i=0; i<allTextLines.length; i++){
+      var data = allTextLines[i].split(',');
+      var tarr = [];
+      for (var j=0; j<data.length; j++){
+        tarr.push(data[j]);
+      }
+      mainData.push(tarr);
+    }
+    mainReader(mainData);
+  }
 
+function mainReader(list){
+    for (i=0;i<list.length;i++){
 
+        let hadEmail = false;
+        let hadSMS = false;
 
+        if(checkVIP(list[i][0])){
+            // Check if can receive email
+            if(list[i][6] == "yes" && list[i][5] != ""){
+                writeEmail(list[i][0],list[i][5],list[i][8],AM_NUMBERS.list[i][8],makeAMEmail(list[i][8]));
+                hadEmail = true;
+            }
+            // Check if can receive sms
+            if(list[i][4] == "yes" && list[i][3] != ""){
+                writeNumber(list[i][3]);
+                hadSMS = true;
+            }
+            if(hadEmail == true || hadSMS == true){
+                writeLetter(list[i][0],list[i][1],list[i][2]);
+            }
+        }
+    }
+}
 
+// Check line is VIP
+function checkVIP(name){
+    for(i=0;1<VIP_NAMES;i++){
+        if(name == VIP_NAMES[i]){
+            return false;
+        }
+    }
+    return true;
+}
 
+// Check correct AM (replace if needed)
+function checkAM(am){
+    for(i=0;i<ACCOUNT_MANAGERS;i++){
+        if(am == ACCOUNT_MANAGERS[i]){
+            return am;
+        }
+    }
+    return "Jane Eveleigh";
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Make AM Email
+function makeAMEmail(am){
+    let amSplit = am.split(" ");
+    return amSplit[0] + amSplit[1] + "@localpages.co.uk"; 
+}
 
 // Functions to write rows to the tables
 function writeEmail (company,email,am,amNumber,amEmail){
